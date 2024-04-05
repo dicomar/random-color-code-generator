bc05ce048ffae2368129bcf377dff3cd39926a64
dicomar <127745122+dicomar@users.noreply.github.com>
2024-04-05 18:15:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const btn = document.getElementById("genCode");
-let r, g, b, rgbColor, rHex, gHex, bHex, hexCode, hexColor;
+const btn = document.getElementById("btnGenerateCode");
+const hexCode = document.getElementById("hexCode");
+const rgbCode = document.getElementById("rgbCode");
+const colorDisplay = document.getElementById('colorDisplay')
+
+let r, g, b, rgbColor, rHex, gHex, bHex, hex, hexColor;
 
 const toHexColor = (num) => {
   // function to convert given number to hexadecimal number code
-  hexCode = parseInt(num).toString(16);
-  if (hexCode.length == 1) {
-    return "0" + hexCode;
+  hex = parseInt(num).toString(16);
+  if (hex.length == 1) {
+    return "0" + hex;
   } else {
-    return hexCode;
+    return hex;
   }
 };
 
-btn.onclick = () => {
+btnGenerateCode.onclick = () => {
   /* randomly generating red, green and blue value, then converting 
   the value to hexadecimal number code */
   r = Math.floor(Math.random() * 256);
@@ -24,6 +28,11 @@ btn.onclick = () => {
 
   rgbColor = `rgb(${r}, ${g}, ${b})`;
   hexColor = `#${rHex}${gHex}${bHex}`;
+
+  rgbCode.innerText = rgbColor;
+  hexCode.innerText = hexColor;
+
+  colorDisplay.style.backgroundColor = hexColor
 
   console.log(rgbColor, hexColor);
 };
